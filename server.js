3ee5be262c913cefaf948ed11f8dd33b04b6c71c
Pete Petrash <pete.petrash@gmail.com>
2015-09-07 00:00:08 -0700

    functioning

--- server.js
+++ server.js
@@ -2,14 +2,15 @@ var fs = require('fs');
 var path = require('path');
 var express = require('express');
 var bodyParser = require('body-parser');
+var app = express();
+
 var webpack = require('webpack');
 var config = require('./webpack.config');
-var app = express();
 var compiler = webpack(config);
 
 app.set('port', (process.env.PORT || 3000));
 
-app.use('/', express.static(path.join(__dirname, '')));
+app.use('/', express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(require('webpack-dev-middleware')(compiler, {
@@ -36,16 +37,8 @@ app.post('/foods.json', function(req, res) {
   });
 });
 
-app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-app.listen(3000, 'localhost', function (err) {
-  if (err) {
-    console.log(err);
-    return;
-  }
+app.listen(app.get('port'), function() {
+  console.log('Foodbase running at localhost:' + app.get('port') + '/');
   var spawn = require('child_process').spawn
   spawn('open', ['http://localhost:' + app.get('port')]);
-  console.log('Listening at http://localhost:3000');
 });
--- src/colors.js
+++ src/colors.js
@@ -1,2 +1,2 @@
-export const NICE = 'green';
-export const SUPER_NICE = 'orange';
+export const NICE = 'aqua';
+export const SUPER_NICE = 'purple';
