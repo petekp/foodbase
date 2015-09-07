e485c4f3f407758cf69fbcb14d71d53145092ea7
Pete Petrash <pete.petrash@gmail.com>
2015-09-06 18:08:36 -0700

    up and running

--- server.js
+++ server.js
@@ -2,12 +2,21 @@ var fs = require('fs');
 var path = require('path');
 var express = require('express');
 var bodyParser = require('body-parser');
+var webpack = require('webpack');
+var config = require('./webpack.config');
 var app = express();
+var compiler = webpack(config);
 
 app.set('port', (process.env.PORT || 3000));
-app.use('/', express.static(path.join(__dirname, 'public')));
+
+app.use('/', express.static(path.join(__dirname, '')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(require('webpack-dev-middleware')(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath
+}));
+app.use(require('webpack-hot-middleware')(compiler));
 
 app.get('/foods.json', function(req, res) {
   fs.readFile('foods.json', function(err, data) {
@@ -27,8 +36,16 @@ app.post('/foods.json', function(req, res) {
   });
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Foodbase running at localhost:' + app.get('port') + '/');
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(3000, 'localhost', function (err) {
+  if (err) {
+    console.log(err);
+    return;
+  }
   var spawn = require('child_process').spawn
   spawn('open', ['http://localhost:' + app.get('port')]);
+  console.log('Listening at http://localhost:3000');
 });
