7b48f12dd68961b6e1ad7d28a74eaad5408d4b29
Pete Petrash <pete.petrash@gmail.com>
2015-09-29 20:20:31 -0700

    fix router

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { Router, Route, Link } from 'react-router';
 import { Foodbase, NavPrimary } from './App';
 import { Login } from './Login';
-import createBrowserHistory from 'history/lib/createBrowserHistory'
-let history = createBrowserHistory()
 
-const routes = {
-  <Route path="/" handler={Foodbase}/>
-  <Route path="login" handler={Login}/>
-}
+const routes = [
+  {
+    path: '/',
+    component: Foodbase
+  }
+]
 
 React.render(
     <Router routes={routes} />, document.getElementById('root')
