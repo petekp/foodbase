3873bb0654ebb17a7dcf29229840c4b6c9164661
Pete Petrash <pete.petrash@gmail.com>
2015-11-12 22:18:42 -0800

    t-comb forms working

--- app/components/Admin/Admin.jsx
+++ app/components/Admin/Admin.jsx
@@ -1,9 +1,35 @@
 import React, { Component } from 'react'
+var t = require('tcomb-form');
+var Form = t.form.Form;
 
-export default class Admin extends Component {
+// define your domain model with tcomb
+// https://github.com/gcanti/tcomb
+var Person = t.struct({
+  name: t.String,
+  surname: t.String
+});
+
+var Admin = React.createClass( {
+    save() {
+        // call getValue() to get the values of the form
+        var value = this.refs.form.getValue();
+        // if validation fails, value will be null
+        if (value) {
+          // value here is an instance of Person
+          console.log(value);
+        }
+    },
     render() {
-        return(
-            <div>admin buddy</div>
-        )
-    }
-}
+        return (
+          <div>
+            <Form
+              ref="form"
+              type={Person}
+            />
+            <button onClick={this.save}>Save</button>
+          </div>
+        );
+      }
+})
+
+module.exports = Admin;
