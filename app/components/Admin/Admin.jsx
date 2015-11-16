264cbcd767750732114ed3eafc08e19a7c21a4c4
Pete Petrash <pete.petrash@gmail.com>
2015-11-15 19:41:39 -0800

    trying stuff with forms and Parse

--- app/components/Admin/Admin.jsx
+++ app/components/Admin/Admin.jsx
@@ -1,32 +1,17 @@
 import React, { Component } from 'react'
-var t = require('tcomb-form');
-var Form = t.form.Form;
-
-// define your domain model with tcomb
-// https://github.com/gcanti/tcomb
-var Person = t.struct({
-  name: t.String,
-  surname: t.String
-});
+import FoodCreateForm from '../FoodCreateForm/FoodCreateForm'
 
 var Admin = React.createClass( {
+    componentWillMount() {
+
+    },
     save() {
-        // call getValue() to get the values of the form
-        var value = this.refs.form.getValue();
-        // if validation fails, value will be null
-        if (value) {
-          // value here is an instance of Person
-          console.log(value);
-        }
+
     },
     render() {
         return (
           <div>
-            <Form
-              ref="form"
-              type={Person}
-            />
-            <button onClick={this.save}>Save</button>
+            <FoodCreateForm />
           </div>
         );
       }
---
+++ app/components/FoodCreateForm/FoodCreateForm.jsx
@@ -0,0 +1,70 @@
+import React, { Component } from 'react'
+import Parse from 'parse'
+import ParseReact from 'parse-react'
+import './FoodCreateForm.css'
+
+var ParseComponent = ParseReact.Component(React)
+Parse.initialize('agvA5VJCcRs9KrikUD0bcrS4D2WaqiKaO35ZlDhq', 'chYL0LjbqMKCwe4lPeayTt7gTyAP4iXnS7rpND8x')
+
+export default class FoodCreateForm extends ParseComponent {
+    mixins: [ParseReact.Mixin]
+
+    constructor(props) {
+        super(props)
+        this.state = {value: null}
+        this.change = this.change.bind(this)
+    }
+    componentDidMount(props) {
+        this.state.value = "kaaa"
+    }
+    change(event){
+        this.state.value = event.target.value
+        console.log(this.state.value)
+    }
+    observe() {
+        return {
+          months: new Parse.Query('Months'),
+          foods: new Parse.Query('Foods'),
+          locations: new Parse.Query('Locations'),
+          types: new Parse.Query('Types'),
+          seasonality: new Parse.Query('FLM')
+        }
+    }
+    showSeasonalFoods () {
+        let seasonality = this.data.seasonality
+        let inSeasonFoods = seasonality.filter(el =>
+            el.month.name == "May"
+        )
+
+        inSeasonFoods.forEach(i => {
+            // console.log(i.food.name)
+        })
+    }
+    render () {
+        return (
+            <div className="FoodCreateForm">
+                <select size={1 + this.data.foods.length} onChange={this.change} value={this.state.value}>
+                    {this.data.foods.map(function(type) {
+                      return <option id="{type.id}">{type.name}</option>
+                    })}
+                </select>
+                <select size={1 + this.data.types.length}>
+                    <option value="All Foods">All Foods</option>
+                        {this.data.types.map(function(type) {
+                          return <option id="{type.id}">{type.name}</option>
+                        })}
+                </select>
+                <select size={1 + this.data.locations.length}>
+                    {this.data.locations.map(function(location) {
+                      return <option id="{location.id}">{location.name}</option>
+                    })}
+                </select>
+                <select multiple size={1 + this.data.months.length}>
+                    {this.data.months.map(function(month) {
+                      return <option id="{month.id}">{month.name}</option>
+                    })}
+                </select>
+            </div>
+        )
+    }
+}
