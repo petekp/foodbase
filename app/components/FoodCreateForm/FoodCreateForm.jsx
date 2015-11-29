382e00539b63b9d9895e25cf9a05439f58d974a5
Pete Petrash <pete.petrash@gmail.com>
2015-11-29 14:00:07 -0800

    add/remove foods working

--- app/components/FoodCreateForm/FoodCreateForm.jsx
+++ app/components/FoodCreateForm/FoodCreateForm.jsx
@@ -11,18 +11,29 @@ export default class FoodCreateForm extends ParseComponent {
 
     constructor(props) {
         super(props)
-        this.state = {food: null}
-        this.change = this.change.bind(this)
-        this.addFood = this.addFood.bind(this)
+        this.state = {
+            food: null,
+            type: null,
+            location: null,
+            month: null
+        }
     }
-    change(event){
-        this.state.food = event.target.value
-        console.log(this.state.food)
+    changeFood = (e) => {
+        this.state.food = e.target.value
     }
-    addFood() {
+    addFood = () => {
         ParseReact.Mutation.Create('Foods', {
           name: this.refs.newFoodInput.value
         }).dispatch();
+    }
+    removeFood = (food) => {
+        let foods = this.data.foods
+        let selectedFoods = foods.filter(food =>
+            food.name == this.state.food
+        )
+        let id = selectedFoods[0]
+        ParseReact.Mutation.Destroy(id).dispatch();
+        // console.log(selectedFoods[0].objectId)
     }
     observe() {
         return {
@@ -33,25 +44,25 @@ export default class FoodCreateForm extends ParseComponent {
           seasonality: new Parse.Query('FLM')
         }
     }
-    showSeasonalFoods () {
+    showSeasonalFoods() {
         let seasonality = this.data.seasonality
         let inSeasonFoods = seasonality.filter(el =>
             el.month.name == "May"
         )
-        inSeasonFoods.forEach(i => {
-            // console.log(i.food.name)
-        })
     }
     render() {
-
         return (
             <div className="FoodCreateForm">
+
                 <div className="column">
-                    <select size={this.data.foods.length + 1} onChange={this.change} value={this.state.food}>
+                    <select size={this.data.foods.length + 1} onChange={this.changeFood}>
                         {this.data.foods.map(function(food) {
                           return <option key={food.objectId}>{food.name}</option>
                         })}
                     </select>
+                    <p>
+                        <button className="button--red" onClick={this.removeFood} type="button">Remove</button>
+                    </p>
                     <div className="newFoodForm">
                         <input ref="newFoodInput" type="text" defaultValue="Hello!" />
                         <button onClick={this.addFood} type="button">Add</button>
@@ -67,7 +78,7 @@ export default class FoodCreateForm extends ParseComponent {
                 </div>
 
                 <div className="column">
-                    <select size={1 + this.data.locations.length}>
+                    <select size={this.data.locations.length + 1}>
                         {this.data.locations.map(function(location) {
                           return <option key={location.objectId} >{location.name}</option>
                         })}
@@ -75,7 +86,7 @@ export default class FoodCreateForm extends ParseComponent {
                 </div>
 
                 <div className="column">
-                    <select multiple size={1 + this.data.months.length}>
+                    <select multiple size={this.data.months.length + 1}>
                         {this.data.months.map(function(month) {
                           return <option key={month.objectId} >{month.name}</option>
                         })}
--- app/components/FoodList/FoodList.jsx
+++ app/components/FoodList/FoodList.jsx
@@ -3,21 +3,22 @@ import Food from '../Food/Food'
 import sortByKey from '../Helpers/sortByKey'
 
 export default class FoodList extends Component {
+
     constructor(props) {
         super(props)
-        this.state = {food: null}
-    }
-    render() {
-        let foodNodes = sortByKey(this.props.foods, "name").map((food, index) => {
+        this.state = { }
+        this.state.foodNodes = sortByKey(this.props.foods, "name").map( (food, i) => {
             return (
-                <Food id={food.id} key={index} name={food.name} type={food.type} hue={food.hue}>
+                <Food id={food.id} key={i} name={food.name} type={food.type} hue={food.hue}>
                     {food.text}
                 </Food>
             )
         })
+    }
+    render() {
         return (
             <div className="FoodList">
-                {foodNodes}
+                {this.state.foodNodes}
             </div>
 
         )
