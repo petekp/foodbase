202c9ffc3e611ef746008800870f89517020aadf
Pete Petrash <pete.petrash@gmail.com>
2015-12-05 18:51:09 -0800

    add location->month relation

--- app/components/FoodCreateForm/FoodCreateForm.jsx
+++ app/components/FoodCreateForm/FoodCreateForm.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Parse from 'parse'
 import ParseReact from 'parse-react'
+import getUniqueArray from '../Helpers/getUniqueArray'
 import './FoodCreateForm.css'
 
 var ParseComponent = ParseReact.Component(React)
@@ -11,19 +12,20 @@ export default class FoodCreateForm extends ParseComponent {
     constructor(props) {
         super(props)
         this.state = {
-            food: "Apples",
             type: "Veggies",
             location: "Alabama",
-            month: ["September"]
+            months: [],
+            food: "Apples",
+            seasonalFoods: []
         }
     }
     observe(props, state) {
         return {
-          months: new Parse.Query('Months'),
-          foods: new Parse.Query('Foods').include('type'),
-          locations: new Parse.Query('Locations'),
-          types: new Parse.Query('Types'),
-          seasonality: new Parse.Query('FLM')
+            types: new Parse.Query('Types'),
+            locations: new Parse.Query('Locations'),
+            months: new Parse.Query('Months'),
+            foods: new Parse.Query('Foods').include('type'),
+            FLM: new Parse.Query('FLM')
         }
     }
     addFood = () => {
@@ -54,6 +56,7 @@ export default class FoodCreateForm extends ParseComponent {
         ParseReact.Mutation.Destroy(id).dispatch();
     }
     changedFood = (e) => {
+        this.getSeasonalFoods()
         this.setState({food : e.target.value})
 
         let foods = this.data.foods
@@ -66,66 +69,87 @@ export default class FoodCreateForm extends ParseComponent {
         this.setState({type : e.target.value})
     }
     changedLocation = (e) => {
-        this.setState({location : e.target.value})
+        let FLM = this.data.FLM
+        let months = FLM.filter(obj =>
+            obj.location.name == e.target.value
+        ).map((obj) => {
+            return obj.month.name
+        })
+        let uniqueMonths = getUniqueArray(months);
+        this.setState({location : e.target.value, months : uniqueMonths})
     }
     changedMonth = (e) => {
-        this.setState({month : e.target.value})
+        this.setState({months : e.target.value})
     }
-    showSeasonalFoods() {
-        let seasonality = this.data.seasonality
-        let inSeasonFoods = seasonality.filter(el =>
-            el.month.name == "May"
+    getSeasonalFoods() {
+        let FLM = this.data.FLM
+        let seasonalFoods = FLM.filter(el =>
+            el.month.name == this.state.months
         )
+        this.setState({seasonalFoods : seasonalFoods})
     }
     render() {
         var food = this.state.food,
             type = this.state.type,
             location = this.state.location,
-            month = this.state.month
+            months = this.state.months
 
         return (
             <div className="FoodCreateForm">
-                <div className="actions">
-                    <input ref="newFoodInput" type="text" defaultValue="" placeholder="Food" />
-                    <button onClick={this.addFood} type="button">Add</button>
-                    <button className="button--warning" onClick={this.removeFood} type="button">Remove</button>
-                </div>
+                <section>
+                    <div className="actions">
+                        <input ref="newFoodInput" type="text" defaultValue="" placeholder="Food" />
+                        <button onClick={this.addFood} type="button">Add</button>
+                        <button className="button--warning" onClick={this.removeFood} type="button">Remove</button>
+                    </div>
 
-                <div className="foods column">
-                    <h2>Foods</h2>
-                    <select value={food} onChange={this.changedFood} size={this.data.foods.length + 1}>
-                        {this.data.foods.map(function(food) {
-                          return <option key={food.objectId}>{food.name}</option>
-                        })}
-                    </select>
-                </div>
+                </section>
+                <section>
+                    <div className="types column">
+                        <h2>Types</h2>
+                        <select value={type} onChange={this.changedType} size={this.data.types.length + 1}>
+                            {this.data.types.map(function(type) {
+                              return <option key={type.objectId} >{type.name}</option>
+                            })}
+                        </select>
+                    </div>
 
-                <div className="types column">
-                    <h2>Types</h2>
-                    <select value={type} onChange={this.changedType} size={this.data.types.length + 1}>
-                        {this.data.types.map(function(type) {
-                          return <option key={type.objectId} >{type.name}</option>
-                        })}
-                    </select>
-                </div>
+                    <div className="foods column">
+                        <h2>Foods</h2>
+                        <select value={food} onChange={this.changedFood} size={this.data.foods.length + 1}>
+                            {this.data.foods.map(function(food) {
+                              return <option key={food.objectId}>{food.name}</option>
+                            })}
+                        </select>
+                    </div>
 
-                <div className="locations column">
-                    <h2>Locations</h2>
-                    <select value={location} onChange={this.changedLocation} size={this.data.locations.length + 1}>
-                        {this.data.locations.map(function(location) {
-                          return <option key={location.objectId} >{location.name}</option>
-                        })}
-                    </select>
-                </div>
+                    <div className="locations column">
+                        <h2>Locations</h2>
+                        <select value={location} onChange={this.changedLocation} size={this.data.locations.length + 1}>
+                            {this.data.locations.map(function(location) {
+                              return <option key={location.objectId} >{location.name}</option>
+                            })}
+                        </select>
+                    </div>
 
-                <div className="months column">
-                    <h2>Months</h2>
-                    <select value={month} onChange={this.changedMonth} size={this.data.months.length + 1}>
-                        {this.data.months.map(function(month) {
-                          return <option key={month.objectId} >{month.name}</option>
+                    <div className="months column">
+                        <h2>Months</h2>
+                        <select value={months} multiple={true} onChange={this.changedMonth} size={this.data.months.length + 1}>
+                            {this.data.months.map(function(month) {
+                              return <option key={month.objectId} >{month.name}</option>
+                            })}
+                        </select>
+                    </div>
+
+
+                </section>
+                <section>
+                    <div className="selections">
+                        {this.state.type} • {this.state.food} •  {this.state.location} •  {this.state.months} •  {this.state.seasonalFoods.map(function(food) {
+                          return <div>{food.food.name}</div>
                         })}
-                    </select>
-                </div>
+                    </div>
+                </section>
             </div>
         )
     }
---
+++ app/components/Helpers/getUniqueArray.js
@@ -0,0 +1,3 @@
+export default function getUniquesArray(array) {
+    return [...new Set(array)]
+}
